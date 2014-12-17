32e188b6d8bf8766bc0dd3355cecaebb75d92917
Igor Kupczyński <igor@kupczynski.info>
2014-12-17 21:26:24 +0100

    Working on the utility

--- status-checker.js
+++ status-checker.js
@@ -1,39 +1,55 @@
 var request = require('request');
 
-var serverCallback = function(topLevel, address, status) {
-  return function(error, response, body) {
-    if (!error) {
-      if (response.statusCode == status) {
-        console.log("OK :: Connected to " + address + ", with status: " +
-          response.statusCode);
-      } else {
-        console.log("ERROR :: Connected to " + address +
-          ", but with wrong status:  " + response.statusCode +
-          ", expected: " + status);
-      }
-    } else {
-      console.log("ERROR :: Can't connect to " + address + ", reason: " +
-        error);
-    }
-    setImmediate(topLevel);
-  };
+var serverCallback = function(address, status) {
+    return function(error, response, body) {
+        if (!error) {
+            if (response.statusCode == status) {
+                results.push({
+                    "status": "CONNECTED_OK",
+                    "address": address,
+                    "got": response.statusCode,
+                    "expected": status
+                });
+            } else {
+                results.push({
+                    "status": "CONNECTED_WRONG_STATUS",
+                    "address": address,
+                    "got": response.statusCode,
+                    "expected": status
+                });
+            }
+        } else {
+            results.push({
+                "status": "CAN'T CONNECT",
+                "address": address,
+                "got": error.code,
+                "expected": status
+            });
+        }
+        setImmediate(callAddresses);
+    };
 };
+
+var done = function() {
+    console.log(results);
+}
 
 var callAddresses = function() {
-  var address = addresses.pop();
-  if (address) {
-    request(address, serverCallback(callAddresses, address, servers[address]));
-  }
+    var address = addresses.pop();
+    if (address) {
+        request(address, serverCallback(address, servers[address]));
+    } else {
+        done();
+    }
 };
 
-
-
 var argv = require('optimist')
-  .usage('Usage: $0 -s [server-list.json]')
-  .demand(['s'])
-  .argv;
+        .usage('Usage: $0 -s [server-list.json]')
+        .demand(['s'])
+        .argv;
 
 var servers = require("./" + argv.s);
+var results = [];
 
 var addresses = Object.keys(servers);
 
